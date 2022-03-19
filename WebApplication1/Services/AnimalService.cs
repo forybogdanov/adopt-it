8546ec9456a82f3ae65e476a03b45cdce931f40c
Nikifor <nikiforn04@gmail.com>
2022-03-19 11:57:53 +0200

    Add documentation for AnimalService, PostService

--- WebApplication1/Services/AnimalService.cs
+++ WebApplication1/Services/AnimalService.cs
@@ -7,6 +7,10 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Services
 {
+    //
+    //Summary:
+    //  Implements CRUD operations with the DB for the Class Animal
+    //
     public class AnimalService : IAnimalService
     {
         private UserDbContext dbContext;
@@ -15,20 +19,28 @@ namespace WebApplication1.Services
         {
             this.dbContext = dbContext;
         }
-
-
+        //
+        //Summary:
+        //  Base method for the AnimalService used to Create a new instance of the class Animal and update the DB
+        //
         public void Create(Animal animal)
         {
             dbContext.Animals.Add(animal);
             dbContext.SaveChanges();
         }
-
+        //
+        //Summary:
+        //    Base method for the AnimalService used to Delete an instance of the class Animal and remove it from the DB
+        //
         public void Delete(int id)
         {
             dbContext.Animals.Remove(GetById(id));
             dbContext.SaveChanges();
         }
-
+        //
+        //Summary:
+        //    Base method for the AnimalService used to Edit an instance of the class Animal and update the DB
+        //
         public void Edit(Animal animal)
         {
             Animal oldAnimal = GetById(animal.Id);
@@ -38,7 +50,10 @@ namespace WebApplication1.Services
             oldAnimal.Status = animal.Status;
             dbContext.SaveChanges();
         }
-
+        //
+        //Summary:
+        //    Base method for the AnimalService used to find an instance of the class Animal in the DB Context by Id
+        //
         public Animal GetById(int id)
         {
             return dbContext.Animals.FirstOrDefault(p => p.Id == id);
