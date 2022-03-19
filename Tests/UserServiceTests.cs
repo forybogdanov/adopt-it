bb3075496cc18739821ced26d6a4df55633d580b
GerganaSavcheva <gerganasvch@abv.bg>
2022-03-19 02:31:31 +0200

    implements nunit tests

--- Tests/UserServiceTests.cs
+++ Tests/UserServiceTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using WebApplication1.Data;
 using WebApplication1.Models;
 using WebApplication1.Models.DTOs;
 using WebApplication1.Services;
@@ -13,63 +16,86 @@ namespace Tests
     public class UserServiceTests
     {
         private UserService userService;
+        private UserDbContext context;
 
         private User user;
 
-        public UserServiceTests()
-        {
-
-
-            this.userService = userService;
-                //= new UserService(dbContext);
-        }
 
         [SetUp]
         public void Setup()
         {
+
+            var options = new DbContextOptionsBuilder<UserDbContext>()
+               .UseInMemoryDatabase("TestDb").Options;
+
+            this.context = new UserDbContext(options);
+            userService = new UserService(this.context);
+
+
+
             this.user = new User();
+            user.Id = 1;
+            user.Email = "asd@asd.com";
+            context.Users.Add(user);
+            context.SaveChanges();
+
         }
 
         [Test]
-        public void Update()
+        public void TestUpdate()
         {
-
-            Assert.Throws<System.ArgumentException>(() => user.UserName = "");
-        }
-
-        [Test]
-        public void Delete()
-        {
-
-            Assert.Throws<System.ArgumentException>(() => user.UserName = "");
-        }
-        [Test]
-        public void GetEntityById()
-        {
-
-            Assert.Throws<System.ArgumentException>(() => user.UserName = "");
-        }
-        [Test]
-        public void GetById()
-        {
-            User user = new User();
 
             UserDTO userDTO = new UserDTO();
+            userDTO.FirstName = "Alex";
+            userDTO.LastName = "Ivanova";
+            userDTO.City = "Sofia";
+            userDTO.PhoneNumber = "0888888888";
+
+            userService.Update(user.Id, userDTO);
+
+            User userDB = context.Users.FirstOrDefault(u => u.Id == user.Id);
+
+            Assert.NotNull(userDB);
+            Assert.AreEqual(userDB.FirstName, "Alex");
+            Assert.AreEqual(userDB.LastName, "Ivanova");
+            Assert.AreEqual(userDB.City, "Sofia");
+            Assert.AreEqual(userDB.PhoneNumber, "0888888888");
 
 
-            Assert.Throws<System.ArgumentException>(() => user.UserName = "");
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+
+            userService.Delete(user.Id);
+
+            User userDB = context.Users.FirstOrDefault(u => u.Id == user.Id);
+
+            Assert.Null(userDB);
         }
         [Test]
-        public void toDTO()
+        public void TestGetEntityById()
         {
-            User user = new User();
-            
-            UserDTO userDTO= new UserDTO();
 
-            // ?? toDTO() e private
+            User userDB = userService.GetEntityById(user.Id);
+
+            Assert.AreEqual(userDB.Email, "asd@asd.com");
+        }
+        [Test]
+        public void TestGetById()
+        {
+            UserDTO userDB = userService.GetById(user.Id);
+
+            Assert.AreEqual(userDB.Email, "asd@asd.com");
         }
 
 
+        [TearDown]
+        public void TearDown()
+        {
+            this.context.Database.EnsureDeleted();
+        }
 
     }
 }
